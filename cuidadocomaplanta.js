7eacf3f7bf6d5480ec13732d4408214541d73a18
pripac <pristudy96@gmail.com>
2021-09-28 13:36:20 -0300

    Modificação no texto

---
+++ cuidadocomaplanta.js
@@ -0,0 +1,11 @@
+alert("ATENÇÃO! Esse jogo foi desenvolvido com o objetivo de popularizar as PANCs (Plantas Alimentícias Não Convencionais), além de alertar para a necessidade de sua devida identificação para o consumo. SEMPRE identifique a espécie pelo nome cientifico antes de comer para evitar intoxicações.")
+
+function comerCharuto() {
+    alert("Infelizmente você foi envenenado e morreu. Pesquise melhor da próxima vez!");
+alert("GAME OVER")
+alert("Nova rodada")
+}
+
+function comerPANC() {
+    alert("Parabéns! A planta escolhida é uma Planta Alimentícia Não Convencional. Não é incrível que dá pra comer os matinhos que crescem por ai?");
+}
